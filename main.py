199e352449c79cda9b7578067084bb6606e546d2
Murillo Pinheiro de Oliveira <murillop.o06@gmail.com>
2024-06-11 11:32:02 -0300

    updated: pronto para apresentar

--- main.py
+++ main.py
@@ -1,93 +1,160 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from typing import List
+from typing import List, Annotated
+import sql_app.models as models
+from sql_app.database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from leitorPdf import LeitorFinal
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class PdfContentBase(BaseModel):
+    userId: int
+    dataEnvio: int
+    text: str
+
+class UserBase(BaseModel):
+    email: str
+    senha: str
+    nome_user: str
+    username: str
+    foto: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def search_user(list, search_value):
+    for i in list:
+        if i == "":
+            return i
+    return None 
+
 
 class Fine_content(BaseModel):
-    name: str
     lastModified: int
     text: str
 
 class Traffic_violation(BaseModel):
     id: int
-    titulo_pdf: str
-    data_envio: int
-    id_user: int
-    nome_condutor: str
     nome_pessoa: str
-    cpf: str
     placa_veiculo: str
+    marca_veiculo: str
+    especie: str
     infracao: str
-    gravidade: str
+    natureza: str
+    pontuacao: str
+    data_envio: int
     data_infracao: str
     hora_infracao: str
     endereco_infracao: str
+    velocidade_regulamentada: str
+    velocidade_media: str
+    velocidade_considerada: str
+    enquadramento: str
+    artigo_ctb: str
+    numero_ait: str
+    data_limite_ind_condutor: str
+    nro_infraest: str
+    identificacao_equipamento: str
+    afericao_certificacao: str
+    agente_transito: str
+
 
 class User(BaseModel):
     id: int
-    name: str
-    emai: str
-
-fine: Traffic_violation = {
-    "id": 12345,
-    "titulo_pdf": "Pdf multa",
-    "data_envio": 1626739200000 ,
-    "id_user": 1,
-    "nome_condutor": "João Silva",
-    "nome_pessoa": "Maria Santos",
-    "cpf": "123.456.789-00",
-    "placa_veiculo": "ABC-1234",
-    "infracao": "Excesso de velocidade",
-    "gravidade": "Grave",
-    "data_infracao": "21/04/2019",
-    "hora_infracao": "10:30:00",
-    "endereco_infracao": "Rua das Flores, 123 - Centro, São Paulo/SP"
-}
+    email: str
+    senha: str
+    nome_user: str
+    username: str
+    foto: str
 
 users: List[User] = [
     { 
-        "id": 1,
-        "name": "Hermann Hesse",
         "email": "lobao@gmail.com",
+        "senha": "12345678",
+        "nome_user": "Hermann Hesse",
+        "username": "HermannHesse",
+        "foto": "asdasdas"
     },
     { 
-        "id": 2,
-        "name": "Erik Satie",
         "email": "erikaodms@gmail.com",
+        "senha": "12345678",
+        "nome_user": "Erik Satie",
+        "username": "ErikSatie",
+        "foto": "asdasdas"
     },
-     { 
-        "id": 3,
-        "name": "Gustave Dore",
+    { 
         "email": "sorime@gmail.com",
+        "senha": "12345678",
+        "nome_user": "Gustave Dore",
+        "username": "GustaveDore",
+        "foto": "asdasdas"
     }
 ]
 
-@app.post("/fines")
-async def post_fine_text(fine_text: List[Fine_content]):
-    return { "msg": "sucess", "code": 200 }
+@app.post("/fines", status_code=status.HTTP_201_CREATED)
+async def post_fine_data(fines_data: List[PdfContentBase], db: db_dependency):
+    for fine in fines_data: 
+        fine_values = LeitorFinal.main(fine.text)
+        # return fine_values
+        db_fine = models.PDF(**fine_values, user_id=fine.userId, 
+        data_envio=fine.dataEnvio)
+        db.add(db_fine)
+        db.commit()
+        db.refresh(db_fine)
+    return { "code": 200, "msg": "success" }
 
-@app.get("/fines/{fine_id}")
-async def get_fine(fine_id: int):
-    # do logic to communicate with the backend
-    return fine
-
-@app.get("/fines")
-async def get_fines(id_user: int | None = None):
-    # do logic to communicate with the backend
+@app.get("/fines", status_code=status.HTTP_200_OK)
+async def get_fines(db: db_dependency, id_user: int | None = None, username: str = None):
+    fines = []
     if (id_user):
-        pass
+        fines = db.query(models.PDF).filter(models.PDF.user_id == id_user).all()
+    elif (username):
+        user = db.query(models.User).filter(models.User.username == username).first()
+        if (user):
+            fines = db.query(models.PDF).filter(models.PDF.user_id == user.id_user).all()
+        else:
+            raise HTTPException(status_code=404, detail='Usuario não encontrado')
     else:
-        pass
+        fines = db.query(models.PDF).all()
 
-    fines = [fine, fine, fine, fine]
-
+    if (len(fines) == 0):
+        raise HTTPException(status_code=404, detail='Nenhum documento foi encontrado')
     return fines
 
-@app.get("/users/{user_id}")
-async def get_user(user_id: int):
-    return users[0]
+@app.get("/users", status_code=status.HTTP_200_OK)      
+async def get_user(db: db_dependency, user_id: int = None, username: str = None):
+    user = None
+    if (user_id):
+        user = db.query(models.User).filter(models.User.id_user == user_id).first()
+    elif (username):
+        user = db.query(models.User).filter(models.User.username == username).first()
+    else:
+        user = db.query(models.User).all()
+    
+    if user is None:
+        raise HTTPException(status_code=404, detail='Usuario não encontrado')
+    
+    return user
+        
 
-@app.get("/users")
-async def get_user():
-    return users
+# @app.get("/fines/{fine_id}")
+# async def get_fine(fine_id: int):
+#     # do logic to communicate with the backend
+#     return fine
+
+# @app.post("/users/", status_code=status.HTTP_201_CREATED)
+# async def create_user(user: UserBase, db: db_dependency):
+#     db_user = models.User(**user.model_dump())
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+#     return db_user
---
+++ sql_app/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import String, Column, ForeignKey, Integer, Date, BigInteger
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class PDF(Base):
+    __tablename__ = 'pdf'
+    id_pdf = Column(Integer, primary_key=True)
+    nome_pessoa = Column(String(30), nullable=False)
+    placa_veiculo = Column(String(20), nullable=False)
+    marca_veiculo = Column(String(20), nullable=False)
+    especie = Column(String(40), nullable=False)
+    infracao = Column(String(200), nullable=False)
+    natureza = Column(String(20), nullable=False)
+    pontuacao = Column(String(20), nullable=False)
+    data_infracao = Column(String(20), nullable=False)
+    hora_infracao = Column(String(300), nullable=False) # 30
+    endereco_infracao = Column(String(200), nullable=False)
+    velocidade_regulamentada = Column(String(20), nullable=False)
+    velocidade_media = Column(String(20), nullable=False)
+    velocidade_considerada = Column(String(20), nullable=False)
+    enquadramento = Column(String(20), nullable=False)
+    artigo_ctb = Column(String(20), nullable=False)
+    numero_ait = Column(String(15), nullable=False)
+    data_limite_ind_condutor = Column(String(20), nullable=False)
+    nro_infraest = Column(String(20), nullable=False)
+    identificacao_equipamento = Column(String(30), nullable=False)
+    afericao_certificacao = Column(String(20), nullable=False)
+    agente_transito = Column(String(1200), nullable=False)
+    data_envio = Column(BigInteger, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id_user"))
+
+    user = relationship("User", back_populates="userData")
+
+class User(Base):
+    __tablename__ = "users" # mudar
+
+    id_user = Column(Integer, primary_key=True)
+    email = Column(String(50), unique=True)
+    senha = Column(String(20))
+    nome_user = Column(String(30))
+    username = Column(String(30), unique=True)
+    foto = Column(String(20))
+
+    userData = relationship("PDF", back_populates="user")
